2eb58200becac7096507c78535a83f1bb3d5a65c
OrisDorch orisdorch@gmail.com <zachamo@hotmail.com>
2023-03-28 10:15:10 -0400

    Updated error management, minor improvements to terminal output, and adjusted the sequencing of functions for a more logical flow.

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Scheduled script to monitor an API-capable Node for outages or stalled chain hei
 
 DESCRIPTION
 Checks node block height -- if no response from node OR no change in height since last check, send alert.
+Cycles through all .env files in the same folder as the script.
 
 SETUP
-Provide relevant user defined variables in the .env file (examples provided)
+Provide relevant user defined variables in the .env file(s) (examples provided)
 Run on a recurring schedule (i.e. every 10 min)
 
 Written for Python v3.9
--- nodemonitor.py
+++ nodemonitor.py
@@ -20,11 +20,59 @@ from email.message import EmailMessage
 import os
 from dotenv import dotenv_values
 
-
 ### Global/Static Variables - do not edit ###
 headers={'content-type':'application/json', 'Accept':'application/json'}  #parameters used in API calls
 
-### API Query Functions ###
+### Check for prior run's output file and load details if found ###
+def lastRunFileCheck():   #Checks for outputs from the last session and updates counters based on these
+    global lastBlock
+    global errorCount
+    print('checking for outputs from last session...')
+    lastBlock = checkFile(blockFilePath)
+    print("   Last known block height is: " + lastBlock)
+    errorCount = checkFile(errorFilePath)
+    print("   Errors since last success: " + errorCount)
+
+## File management processes ## --very basic, for one line files only
+def checkFile(path):
+    try: # try to open the file at the specified path
+        f = open(path, "r")
+        content = f.read()
+        f.close()
+        if content.isdigit():   #Check if file contains  a valid integer
+            return content
+        else:   #If file contents are not integer, ignore file contents and return 0
+            print("   Invalid file contents in "+path+" - using 0...")
+            return "0"
+    except IOError:   #If file cannot be found, create a new empty one and return 0
+        print("   no previous output file has been found at " + path + ", creating a new file and using 0...")
+        f = open(path, "w+")
+        f.close()
+        return "0"
+
+def overwriteFile(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
+    f.close()
+
+### Node Monitor Processes ###
+def checkNodeStatus(nodeAPIURL,endPoint,dataKey):   # primary function of this script
+    height = str(APICallNotPaged(nodeAPIURL,endPoint,dataKey))
+    if height == "None":
+        print('...block height could not be retrieved; proceeding to next node check if applicable...\n')
+    else:
+        print("   Node's current block height is " + height)
+        if int(height) > int(lastBlock):   #height has increased
+            overwriteFile(blockFilePath,height)  #update tracking file with new height
+            if errorCount != "0":
+                print("Prior run's error appears to be resolved, sending update email...")
+                sendEmailAlert("Update: " + nodeChain + " Node is back online"," Your node appears to be back online! \n Its block height has increased from: " + height + " to: " + lastBlock + "You can check the height at: " + nodeAPIURL + endPoint)
+            overwriteFile(errorFilePath,"0")   #set error count to 0
+        else:   #Success -- Height increased since last check - recent errors detected
+            print("###ERROR### Height has not increased since last check - sending alert...")
+            errorHandling("Alert: " + nodeChain + " Node Stuck", "Greetings, \n\nYour node's current block height " + height + " has not increased since last check: " + lastBlock + "\n\nYou can check the height at: " + nodeAPIURL + endPoint)
+
+## API Query Function ##
 # For queries without responses (full contents are returned with a single query) #
 def APICallNotPaged(APINodeURL,endPoint,dataKey):
     requestURL = APINodeURL+endPoint
@@ -36,63 +84,19 @@ def APICallNotPaged(APINodeURL,endPoint,dataKey):
     except requests.exceptions.RequestException as e:   #Catch any exception returned from the request
         print("###ERROR### Node could not be contacted via API call...")
         errorHandling("Alert: " + nodeChain + " Node Offline"," Node could not be contacted due to error: \n" + nodeAPIURL + "\n" + str(e))
-        raise SystemExit(e)   #Exit
 
-### Error Management ### - with error counter and email notifications
-def errorHandling(subject,message):   #Takes the email alert contents, checks error count to see if alert should be supressed, sends alert if warranted.
+## Error Management ## - with error counter and email notifications
+def errorHandling(subject,message):   #Takes the email alert contents, checks error count to see if alert should be suppressed, sends alert if warranted.
     newErrorCount = str(int(errorCount) + 1)
     if newErrorCount == "1" or (int(newErrorCount)/int(realertEvery)).is_integer():  #For notifications -- check if lase run was successful, if no: check if due for a re-alert
-        print ("Sending alert - error number " + newErrorCount)
+        print ("   Sending alert - error number " + newErrorCount)
         sendEmailAlert(subject,message)
-    else: 
-        print ("No Alert - error number " + newErrorCount + ", alerts sent every " + str(realertEvery))
+    else:
+        print ("   No Alert - error number " + newErrorCount + ", alerts sent every " + str(realertEvery))
     overwriteFile(errorFilePath,newErrorCount)   #Update the error counter file
+    print ("   error count updated at: " + errorFilePath)
 
-### File management processes ### --very basic, for one line files only
-def checkFile(path):
-    try: # try to open the file at the specified path
-        f = open(path, "r")
-        content = f.read()
-        f.close()
-        if content.isdigit():   #Check if file contains  a valid integer
-            return content
-        else:   #If file contents are not integer, ignore file contents and return 0
-            print("Invalid file contents in "+path+" - using 0...")
-            return "0"
-    except IOError:   #If file cannot be found, create a new empty one and return 0
-        print("no previous output file has been found at " + path + ", creating a new file and using 0...")
-        f = open(path, "w+")
-        f.close()
-        return "0"
-
-def overwriteFile(path, data):
-    with open(path, 'w') as f:
-        f.write(data)
-    f.close()
-
-
-### Node Monitor Processes ###
-def lastRunFileCheck():   #Checks for outputs from the last session and updates counters based on these
-    global lastBlock
-    global errorCount
-    print('checking for outputs from last session...')
-    lastBlock = checkFile(blockFilePath)
-    print("Last known block height is: " + lastBlock)
-    errorCount = checkFile(errorFilePath)
-    print("Errors since last success: " + errorCount)
-
-def checkNodeStatus(nodeAPIURL,endPoint,dataKey):   # primary function of this script
-    height = str(APICallNotPaged(nodeAPIURL,endPoint,dataKey))
-    print("Node's current block height is " + height)
-    if height <= lastBlock:   #height reduced or unchanged
-        print("###ERROR### Height has not increased since last check - sending alert...")
-        errorHandling("Alert: " + nodeChain + " Node Stuck","Your node's block height " + height + " has not increased since last check: \n" + nodeAPIURL)
-    else:   #Success -- Height increased since last check
-        overwriteFile(blockFilePath,height)  #update tracking file with new height
-        overwriteFile(errorFilePath,"0")   #set error count to 0
-
-
-### Messaging Component for email alerts ###
+## Messaging Component for email alerts ##
 def sendEmailAlert(subject,message): #Plain text messaging only
     msg = EmailMessage()
     msg.set_content(message)
@@ -101,21 +105,20 @@ def sendEmailAlert(subject,message): #Plain text messaging only
     msg['From'] = senderPass
     msg['To'] = recipientAddress
 
-    # Send the message via speified SMTP server
-    print("Sending |" + subject + "| to " + recipientAddress)
-    #Create SMTP session for sending the mail
-    session = smtplib.SMTP(smtpServer,smtpPort) #use gmail with port
+    # Send the message via a specified SMTP server
+    print("   Sending |" + subject + "| to " + recipientAddress)
+    #Create SMTP session for the email
+    session = smtplib.SMTP(smtpServer,smtpPort) #use gmail with specified port
     session.starttls() #enable security
     session.login(senderAddress, senderPass) #login with mail_id and password
     session.send_message(msg)
     session.quit()
-    print("Message sent")
+    print("   Message sent")
 
-    
+
 ### Begin Script Execution ###
 # Load .env files and cycle through them
 env_dir = os.path.dirname(os.path.realpath(__file__))
-
 
 # Find all .env files in the directory
 env_files = [f for f in os.listdir(env_dir) if f.endswith('.env')]
